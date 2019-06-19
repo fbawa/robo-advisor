f49be4554f658808c6e9e348fefb850250da42f3
fbawa <50893833+fbawa@users.noreply.github.com>
2019-06-19 19:56:26 -0400

    Robo1

---
+++ robo_advisor.py
@@ -0,0 +1,35 @@
+# app/robo_advisor.py
+
+import requests
+import json
+
+#information inputs
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
+response = requests.get(request_url)
+#print(type(response)) # class requests.models.response
+#print(response.status_code) #200
+#print(response.text)
+
+parsed_response = json.loads(response.text)
+
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+
+#information outputs
+print("-------------------------")
+print("SELECTED SYMBOL: XYZ")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print(f"LATEST DAY: {last_refreshed}")
+print("LATEST CLOSE: $100,000.00")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")
